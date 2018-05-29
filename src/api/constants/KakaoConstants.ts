126313993542b17f693cc500bb1813e136538a6f
smilu97 <smilup2244@gmail.com>
2018-05-29 13:26:44 +0900

    Fix 'noResultMent'

--- src/api/constants/KakaoConstants.ts
+++ src/api/constants/KakaoConstants.ts
@@ -8,7 +8,7 @@ export const KAKAO = {
         RECEIVING_AREA: 'ReceivingArea',
     },
     confusingMent: '등록되지 않은 검색어 입니다. 다시 입력해주세요',
-    noResultMent: '해당되는 지역에 마케팅이 없습니다',
+    noResultMent: '해당되는 마케팅이 없습니다',
     askFilterMent: '필터를 선택해주세요',
     noImageMent: '사진은 아직 처리하지 않습니다',
     toFirstMent: '필터 초기화',
