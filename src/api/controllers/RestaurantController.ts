b9eb4a1e8aa1101de62291221429b04c0e56829d
smilu97 <smilup2244@gmail.com>
2018-06-07 12:56:14 +0900

    add findAll restaurantEvents

--- src/api/controllers/RestaurantController.ts
+++ src/api/controllers/RestaurantController.ts
@@ -3,16 +3,24 @@ import {
 } from 'routing-controllers';
 import { Restaurant } from '../models/Restaurant';
 import { RestaurantService } from '../services/RestaurantService';
+import { RestaurantEvent } from '../models/RestaurantEvent';
+import { RestaurantEventService } from '../services/RestaurantEventService';
 
 @JsonController('/restaurants')
 export class RestaurantController {
 
     constructor(
-        private restaurantService: RestaurantService
+        private restaurantService: RestaurantService,
+        private restaurantEventService: RestaurantEventService
     ) { }
 
     @Get()
     public find(): Promise<Restaurant[]> {
         return this.restaurantService.find();
+    }
+
+    @Get('/events')
+    public findEvents(): Promise<RestaurantEvent[]> {
+        return this.restaurantEventService.find();
     }
 }
